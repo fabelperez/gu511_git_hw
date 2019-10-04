e0b56744dddfadae45e8f2bf02b748bd9e7a5b8f
fap35 <fap35@georgetown.edu>
2019-10-04 17:39:21 +0000

    helloworld: initial commit
    Signed-off-by: fap35 <fap35@georgetown.edu>

--- helloworld.py
+++ helloworld.py
@@ -2,7 +2,7 @@ import datetime
 
 def hello_world():
     print('Hello world!')
-    now = datatime.datetime.now()
+    now = datetime.datetime.now()
     print('It is {}'.format(now))
 
 if __name__=="__main__":
